c2587025873154105992bcd4b105af82c58b3c88
h4unn <114403611+h4unn@users.noreply.github.com>
2025-01-06 23:52:42 +0900

    Revert "login updatae"

--- src/api/@types/auth.api.d.ts
+++ src/api/@types/auth.api.d.ts
@@ -1,20 +1,20 @@
 type loginRequestBody = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 type loginRequest = {
-  body: loginRequestBody;
+    body: loginRequestBody;
 };
 
 type ILoginResponse = {
-  id: string;
-  email: string;
-  name: string;
+    id: string;
+    email: string;
+    name: string;
 };
 
 type loginResponse = {
-  accessToken: string;
-  //   refreshToken: string;
-  //   user: ILoginResponse;
+    accessToken: string;
+    refreshToken: string;
+    user: ILoginResponse;
 };
